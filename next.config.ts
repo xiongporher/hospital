d6e12476c883375cd0a930bfb66d63f72df518a5
Xinogpor <xiongporher1@gmail.com>
2026-05-27 00:11:39 +0700

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ["social-lizards-report.loca.lt"],
+  output: "export",
 };
 
 export default nextConfig;
